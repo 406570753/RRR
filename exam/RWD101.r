3c91f1f025f1e6231665d9d5b8ad8bc438e78ded
406570753 <80378479+406570753@users.noreply.github.com>
2021-12-08 20:56:20 +0800

    1208

---
+++ exam/RWD101.r
@@ -0,0 +1,14 @@
+# è¼‰å…¥ jsonlite å¥—ä»¶
+library(___)
+
+# è®€?…¥è³‡æ?? read.json
+df <- ___(___)
+
+# ??‡å?šè?é¸??‡å?›å€‹æ?„ä?ç?„å…§å®?
+cols <- c(___, ___, ___, ___)
+
+# ?¸??‡æ?„ä??
+df <- df[, ___]
+
+# ä½¿ç”¨ write.table è¼¸å‡º write.csv
+write.table(___, file = "___", col.names = ___, row.names = ___, sep = ",", ___ = '___')
